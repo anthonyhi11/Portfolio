185e953ce3fdaa7b94885444cb1ba773ade68190
Anthony Hill <antdavhill@gmail.com>
2020-05-01 17:22:36 -0500

    made new styling changes to desktop

--- index.js
+++ index.js
@@ -6,21 +6,23 @@ document.getElementById("take-hike").addEventListener("click", showTakeAHike);
 
 document.getElementById("quiz").addEventListener("click", showQuiz);
 
+document.getElementById("about-me").addEventListener("mouseover", showMe);
+
 function showSaturn() {
-  console.log("clicked");
   document.getElementById("project").innerHTML = saturn;
 }
 
 function showWhibut() {
-  console.log("clicked");
   document.getElementById("project").innerHTML = whibut;
 }
 
 function showTakeAHike() {
-  console.log("clicked");
   document.getElementById("project").innerHTML = takeAHike;
 }
 function showQuiz() {
-  console.log("clicked");
   document.getElementById("project").innerHTML = quiz;
 }
+
+// function showMe() {
+//   document.getElementById("profile-pic").innerHTML = currentMe;
+// }
--- store.js
+++ store.js
@@ -1,42 +1,184 @@
-const quiz = `<h3 class="project-title">Bon Iver Quiz App</h3>
+const quiz = `<div class="project-container">
+<img
+  src="Images/desktop-mockup.png"
+  alt="app-mockup"
+  class="desktop-mockup"
+/>
+<div class="project-info-container">
+  <h2 class="project-name">Bon Iver Quiz</h2>
   <div class="tech-used">
-  <img src="Images/icons8-html-5-50.png" class="tech" alt="html5" />
-  <img src="Images/css-3.png" class="tech" alt="CSS3" />
-  <img src="Images/icons8-javascript-50.png" class="tech" alt="JavaScript"/>
-  <img src="images/icons8-jquery-50.png" class="tech" alt="jQuery" />
+    <img
+      src="Images/icons8-html-5-50.png"
+      class="tech"
+      alt="html5"
+    />
+    <img src="Images/css-3.png" class="tech" alt="CSS3" />
+    <img
+      src="Images/icons8-javascript-50.png"
+      class="tech"
+      alt="JavaScript"
+    />
+    <img
+      src="images/icons8-jquery-50.png"
+      class="tech"
+      alt="jQuery"
+    />
   </div>
-  <div class="projects-flex">
-  <p>
-          The goal of this application is to have a user be tested on their
-          knowledge of my favorite band, Bon Iver. As the user progresses
-          through the quiz, they are presented with a question and 4 options.
-          <img src="Images/options-quiz.png" class="images" />
-          Once an option is selected and submitted, they are shown if they are
-          correct or incorrect. If incorrect, they can show the correct answer
-          or continue on to the next question.
-          <img src="Images/incorrect-quiz.png" class="images" />
-          The score is tallied up at the end and they are shown either a
-          congratulations screen or a try again screen depending on the score.
-          The user can then take the quiz again, if they choose.
-          <img src="Images/final-result-quiz.png" class="images" />
-        </p>
-      </div>
-      <h4>Links</h4>
-      <ul class="project-links">
-        <li>
-          <a href="https://anthonyhi11.github.io/quizApp/" target="_blank">
-            Live Page
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/anthonyhi11/quizApp" target="_blank">
-            GitHub Repo
-          </a>
-        </li>
-      </ul>`;
+  <p class="project-description">
+  This was one of the very first projects I worked on. Just a very basic look at DOM 
+  manipulation with jQuery. I had fun thinking of questions and making the quiz difficult... 
+  Try it out if you're a Bon Iver fan!
 
-const whibut = `This is whibut information`;
+  </p>
+  <div class="buttons">
+                <a href="https://github.com/anthonyhi11/quizApp" class="repo-button"target="_blank">REPO</a>
+                <a href="https://anthonyhi11.github.io/quizApp/" target="_blank" class="live-button">LIVE</a>
+              </div>
+</div>
+</div>`;
 
-const takeAHike = `This is Take A Hike information`;
+const whibut = `<div class="project-container">
+<img
+  src="Images/desktop-mockup.png"
+  alt="app-mockup"
+  class="desktop-mockup"
+/>
+<div class="project-info-container">
+  <h2 class="project-name">WHIBUT</h2>
+  <div class="tech-used">
+    <img
+      src="Images/icons8-html-5-50.png"
+      class="tech"
+      alt="html5"
+    />
+    <img src="Images/css-3.png" class="tech" alt="CSS3" />
+    <img
+      src="Images/icons8-javascript-50.png"
+      class="tech"
+      alt="JavaScript"
+    />
+    <img
+      src="images/icons8-jquery-50.png"
+      class="tech"
+      alt="jQuery"
+    />
+  </div>
+  <p class="project-description">
+  WHIBUT - What Have I Been Up To? Is a full-stack application built with the PERN stack. It allows 
+  users to create an account and begin saving activities they have done. Activity categories include, Movies, TV shows, 
+  Restaurants, and Books. It's like your own personal GoodReads, IMDb, YELP all rolled into one. Now you'll never have to google what the name of that
+   random bar was that you found the great cheeseburger deal
+  </p>
+  <div class="buttons">
+                <a href="https://github.com/anthonyhi11/whibut-client" class="repo-button"target="_blank">REPO</a>
+                <a
+  href="https://whibut-client.now.sh/"
+  target="_blank"
+  class="live-button"
+  >LIVE</a
+>
+              </div>
+</div>
+</div>`;
 
-const saturn = "this is saturn!";
+const takeAHike = `<div class="project-container">
+<img
+  src="Images/desktop-mockup.png"
+  alt="app-mockup"
+  class="desktop-mockup"
+/>
+<div class="project-info-container">
+  <h2 class="project-name">Take A Hike</h2>
+  <div class="tech-used">
+    <img
+      src="Images/icons8-html-5-50.png"
+      class="tech"
+      alt="html5"
+    />
+    <img src="Images/css-3.png" class="tech" alt="CSS3" />
+    <img
+      src="Images/icons8-javascript-50.png"
+      class="tech"
+      alt="JavaScript"
+    />
+    <img
+      src="images/icons8-jquery-50.png"
+      class="tech"
+      alt="jQuery"
+    />
+  </div>
+  <p class="project-description">
+  A simple trail finder focused on the bare minimum information a traveling hobbyist needs to know. 
+  Enter the city and state you're in and quickly see the nearest trails with 5 day forecast. This 
+  was an early project giving me experience using multiple external APIs and formatting the data returned.
+  </p>
+  <div class="buttons">
+  <a
+  href="https://github.com/anthonyhi11/take-a-hike"
+  target="_blank"
+  class="repo-button"
+  >REPO</a
+>
+<a
+  href="https://anthonyhi11.github.io/take-a-hike/"
+  target="_blank"
+  class="live-button"
+  >LIVE</a
+>
+              </div>
+</div>
+</div>`;
+
+const saturn = `<div class="project-container">
+<img
+  src="Images/desktop-mockup.png"
+  alt="app-mockup"
+  class="desktop-mockup"
+/>
+<div class="project-info-container">
+  <h2 class="project-name">Saturn</h2>
+  <div class="tech-used">
+    <img
+      src="Images/icons8-html-5-50.png"
+      class="tech"
+      alt="html5"
+    />
+    <img src="Images/css-3.png" class="tech" alt="CSS3" />
+    <img
+      src="Images/icons8-javascript-50.png"
+      class="tech"
+      alt="JavaScript"
+    />
+    <img
+      src="images/icons8-jquery-50.png"
+      class="tech"
+      alt="jQuery"
+    />
+  </div>
+  <p class="project-description">
+    Saturn is a project management tool for remote software
+    development teams. Working on a software project remotely can
+    increase the difficulty of keeping track of feature requests
+    and/or bugs. The typical use case of this application would be a
+    small team of 2-4 developers working remotely. I plan on using
+    Saturn to work on open source projects with other junior
+    developers as I continue my job search.
+  </p>
+  <div class="buttons">
+  <a
+  href="https://github.com/anthonyhi11/saturn-client"
+  target="_blank"
+  class="repo-button"
+  >REPO</a
+>
+<a
+  href="https://saturn-blush.now.sh/"
+  target="_blank"
+  class="live-button"
+  >LIVE</a
+>
+              </div>
+</div>
+</div>`;
+
+let currentMe = `<img src="Images/normal-me.png" alt="picture of me" class="young"/>`;
